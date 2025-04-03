f12d43c254516243f65c1baa22a0b9f1b3087035
Joseph Robles <petter.chuquipiondo.r@uni.pe>
2025-04-03 07:49:51 -0500

    Agregaciòn de icono de Whatsaap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,13 @@ import "@/styles/globals.css";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { motion } from "framer-motion";
+import ChatWidget from "../components/ChatWidget";
+import { FaWhatsapp } from "react-icons/fa"; // Asegúrate de instalar react-icons
 
 export const metadata: Metadata = {
   title: "Productos de cuidado del cabello y cuidado de la piel | Garnier",
-  description: "Garnier ha formulado productos para el cuidado de la piel, coloración, tintes y cuidado del cabello, desodorantes y antitranspirantes para mujer y hombre.",
+  description:
+    "Garnier ha formulado productos para el cuidado de la piel, coloración, tintes y cuidado del cabello, desodorantes y antitranspirantes para mujer y hombre.",
 };
 
 export default function RootLayout({
@@ -22,6 +25,17 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
+
+        {/* Botón de WhatsApp flotante */}
+        <a
+          href="https://wa.me/51959713558?text=Hi
+" // Reemplaza con tu número de WhatsApp
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
+        >
+          <FaWhatsapp size={24} />
+        </a>
       </body>
     </html>
   );
